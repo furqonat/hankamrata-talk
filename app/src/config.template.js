a3e4876347e185ce9ba177dde46478e4396e99f0
furqonat <danioscop11@gmail.com>
2025-12-05 16:30:48 +0700

    room

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -20,8 +20,8 @@ module.exports = {
             title: '<h1>Hankamrata Meet</h1>Free browser based Real-time video calls.<br />Simple, Secure, Fast.',
             description:
                 'Start your next video call with a single click. No download, plug-in, or login is required. Just get straight to talking, messaging, and sharing your screen.',
-            joinDescription: 'Pick a room name.<br />How about this one?',
-            joinButtonLabel: 'JOIN ROOM',
+            joinDescription: 'Pilih Ruangan.',
+            joinButtonLabel: 'MASUK RUANGAN',
             joinLastLabel: 'Your recent room:',
         },
         og: {
